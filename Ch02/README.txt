4aef16bfb38cf66698229ff4fd3ce0d65f094941
snjl <349245467@qq.com>
2019-07-22 22:00:42 +0800

    增加手写体kNN识别

--- Ch02/README.txt
+++ Ch02/README.txt
@@ -1,3 +1,8 @@
-kNN.py 一个简单的测试例子
-kNN_date.py 用1000条约会数据，增加了min-max归一化后的测试例子，参考机器学习第二章
-createFirstPlot.py 绘图
+1. kNN.py 一个简单的测试例子
+2. kNN_date.py 用1000条约会数据，增加了min-max归一化后的测试例子，参考机器学习第二章
+3. createFirstPlot.py 绘图
+4. handWriting.py kNN手写体识别
+
+部分代码参考https://github.com/TrWestdoor/Machine-Learning-in-Action
+
+主要参考https://github.com/pbharrin/machinelearninginaction
---
+++ Ch02/handWriting.py
@@ -0,0 +1,48 @@
+import numpy as np
+import os
+
+from Ch02 import kNN
+
+
+# 读取一张图像,存为一个1024*1的向量
+def img2vector(filename):
+    returnVect = np.zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32*i+j] = int(lineStr[j])
+    return returnVect
+
+
+def handwritingClassTest():
+    # 手写体标签存储
+    hwLabels = []
+    trainingFileList = os.listdir('trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = np.zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        # 获取形式为 标签_训练编号
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
+    testFileList = os.listdir('testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = kNN.classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr))
+        if classifierResult != classNumStr:
+            errorCount += 1.0
+    print("\n the total nubmer of errors is: %d" % errorCount)
+    print("\n the total error rate is: %f" % (errorCount/float(mTest)))
+
+
+handwritingClassTest()
+
